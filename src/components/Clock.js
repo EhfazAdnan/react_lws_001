71eb096008771e3fc2fad026d23139bb078959d0
EhfazAdnan <ehfaz15-167@diu.edu.bd>
2022-08-18 22:02:05 -0700

    7 React Run A Watch Time With State

--- src/components/Clock.js
+++ src/components/Clock.js
@@ -1,14 +1,28 @@
+/* eslint-disable react/no-unused-state */
 /* eslint-disable react/destructuring-assignment */
 /* eslint-disable react/prefer-stateless-function */
 import React from 'react';
 
 // class components
 class Clock extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { date: new Date() };
+    }
+
+    componentDidMount() {
+        setInterval(() => {
+            this.setState({
+                date: new Date(),
+            });
+        }, 1000);
+    }
+
     render() {
         return (
             <h1 className="heading">
                 <span className="text">
-                    Hello it is {new Date().toLocaleTimeString(this.props.locale)}
+                    Hello it is {this.state.date.toLocaleTimeString(this.props.locale)}
                 </span>
             </h1>
         );
